df2f2e6a45a10a06c0aba37f0fddac659da481f5
DestanyM <destanymcclellan22@gmail.com>
2023-07-19 16:24:29 -0400

    Started War Test

---
+++ src/main/java/com/github/zipcodewilmington/warGame.java
@@ -0,0 +1,18 @@
+package com.github.zipcodewilmington;
+
+public class warGame {
+    public void dealCards() {
+    }
+
+    public Object getPlayer1() {
+        return null;
+    }
+
+    public Object getdealer() {
+        return null;
+    }
+
+    public Object getHand(){
+        return null;
+    }
+}
