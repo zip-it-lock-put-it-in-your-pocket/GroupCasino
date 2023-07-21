8031043966ee6e8a45b43166246330e259c1a61b
anudeep-chennuri <anudeep.chennuri@gmail.com>
2023-07-20 21:02:38 -0400

    roulette table progress

--- src/main/java/com/github/zipcodewilmington/MainApplication.java
+++ src/main/java/com/github/zipcodewilmington/MainApplication.java
@@ -1,13 +1,8 @@
 package com.github.zipcodewilmington;
 
-import com.github.zipcodewilmington.casino.games.RideTheBus.RideTheBusGame;
-import com.github.zipcodewilmington.casino.games.slots.SlotsGame;
-
 public class MainApplication {
-    public static void main(String[] args) throws InterruptedException {
+    public static void main(String[] args) {
+
         new Casino().run();
-
-
-
     }
 }
