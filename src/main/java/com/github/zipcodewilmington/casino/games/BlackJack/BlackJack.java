b5397c381b8e5d5969314747acde028c67b22df8
olamide <oadams@fandm.edu>
2023-07-20 22:57:19 -0400

    "update"

---
+++ src/main/java/com/github/zipcodewilmington/casino/games/BlackJack/BlackJack.java
@@ -0,0 +1,20 @@
+package com.github.zipcodewilmington.casino.games.BlackJack;
+import com.github.zipcodewilmington.casino.games.Deck;
+
+public class BlackJack {
+
+    // created variables for the blackjack game class
+    private int wins;
+    private int losses;
+    private int pushes;
+
+//created a constructor for the blackjack game class
+    public BlackJack(){
+        wins = 0;
+        losses = 0;
+        pushes = 0;  //anytime a blackjack object is made it is automatically going to be 0
+    }
+    //create and start the game here
+
+
+}
